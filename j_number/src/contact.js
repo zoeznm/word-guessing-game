a2cb92377126ce2ae82609dbc9220f1e8369b658
zoeznm <bomikim046@gmail.com>
2024-10-12 19:18:52 +0900

    contact 페이지 css 완료

--- j_number/src/contact.js
+++ j_number/src/contact.js
@@ -1,38 +1,45 @@
-import React from 'react';
-import './contact.css'; // CSS 파일 추가
-import { useNavigate } from 'react-router-dom';
-
+import React from "react";
+import "./contact.css"; // CSS 파일 추가
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const handleHome = () => {
-    navigate('/'); // 홈 버튼 클릭 시 메인 페이지로 이동
+    navigate("/"); // 홈 버튼 클릭 시 메인 페이지로 이동
   };
   return (
-    <><h2>Contact</h2>
-    <div className="contact-container">
-      <p>
-        여러분의 의견이나 질문이 있으신가요? 언제든지 저에게 연락해 주세요!
-      </p>
-      <p>
-        이메일: <a href="mailto:example@example.com">example@example.com</a>
-      </p>
-      <p>
-        GitHub: <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">yourusername</a>
-      </p>
-      <p>
-        인스타그램: <a href="https://instagram.com/yourusername" target="_blank" rel="noopener noreferrer">@yourusername</a>
-      </p>
-      <p>
-        여러분의 피드백은 제가 더 나은 콘텐츠를 만드는 데 큰 도움이 됩니다.
-        언제든지 소중한 의견을 기다리고 있습니다. 감사합니다!
-      </p>
-    </div><button
-      onClick={handleHome}
-      className='home'
-    >
-        Home
-      </button></>
+    <div className="container">
+      <>
+        <h2>Contact</h2>
+        <div className="contact-container">
+          <p>제가 궁금하시다면? 아래로 연락주세요 😗</p>
+          <p className="click gmail">
+            <a href="mailto:madlife1292@gmail.com">Gmail</a>
+          </p>
+          <p className="click github">
+            <a
+              href="https://github.com/zoeznm"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              zoeznm
+            </a>
+          </p>
+          <p className="click instagram">
+            <a
+              href="https://instagram.com/matomabo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @matomabo
+            </a>
+          </p>
+        </div>
+        <button onClick={handleHome} className="home">
+          Home
+        </button>
+      </>
+    </div>
   );
 };
 
